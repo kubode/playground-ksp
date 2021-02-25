d0a0d332939d93856bb03bb996cac5c20353de97
Masatoshi Kubode <masatoshi@wantedly.com>
2021-02-25 21:46:32 +0900

    Use Visitor

--- processor/src/main/kotlin/com/github/kubode/playground/ksp/dev/experimental/processor/MyProcessor.kt
+++ processor/src/main/kotlin/com/github/kubode/playground/ksp/dev/experimental/processor/MyProcessor.kt
@@ -8,6 +8,7 @@ import com.google.devtools.ksp.processing.*
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSPropertyDeclaration
 import com.google.devtools.ksp.symbol.KSTypeReference
+import com.google.devtools.ksp.symbol.KSVisitorVoid
 import com.google.devtools.ksp.validate
 
 @AutoService(SymbolProcessor::class)
@@ -37,29 +38,33 @@ class MyProcessor : SymbolProcessor {
         logger.info("props: $props")
         val types = props.map { it.type }.toSet()
         logger.info("types: $types")
-        types.forEach { generateMockByType(it) }
+        types.forEach { it.accept(TypeVisitor(), Unit) }
         val parents = props.mapNotNull { it.parentDeclaration }.toSet()
         logger.info("parents: $parents")
         return invalidSymbols
     }
 
-    private fun generateMockByType(typeRef: KSTypeReference) {
-        val type = typeRef.resolve()
-        val packageName = type.declaration.packageName.getQualifier()
-        val targetFqn = type.declaration.qualifiedName?.asString()!!
-        val targetClassName = type.declaration.simpleName.getShortName()
-        val fileName = "${targetClassName}Mock"
-        val className = "${targetClassName}Mock"
-        val abstractFunctions = type.declaration.closestClassDeclaration()!!.getAllFunctions().filter { it.isAbstract }
-        val abstractProps = type.declaration.closestClassDeclaration()!!.getAllProperties().filter { it.isAbstract() }
-        val output = codeGenerator.createNewFile(
-            dependencies = Dependencies(true),
-            packageName = packageName,
-            fileName = fileName,
-        )
-        // TODO: Improve readability.
-        output.write(
-            """
+    inner class TypeVisitor : KSVisitorVoid() {
+        override fun visitTypeReference(typeReference: KSTypeReference, data: Unit) {
+            logger.info("typeReference: ${typeReference.resolve()}")
+            val type = typeReference.resolve()
+            val packageName = type.declaration.packageName.getQualifier()
+            val targetFqn = type.declaration.qualifiedName?.asString()!!
+            val targetClassName = type.declaration.simpleName.getShortName()
+            val fileName = "${targetClassName}Mock"
+            val className = "${targetClassName}Mock"
+            val abstractFunctions =
+                type.declaration.closestClassDeclaration()!!.getAllFunctions().filter { it.isAbstract }
+            val abstractProps =
+                type.declaration.closestClassDeclaration()!!.getAllProperties().filter { it.isAbstract() }
+            val output = codeGenerator.createNewFile(
+                dependencies = Dependencies(true),
+                packageName = packageName,
+                fileName = fileName,
+            )
+            // TODO: Improve readability.
+            output.write(
+                """
                 package $packageName
                 
                 import $targetFqn
@@ -67,16 +72,20 @@ class MyProcessor : SymbolProcessor {
                 class $className : $targetClassName {
                     
                     ${
-                abstractFunctions
-                    .map { func ->
-                        "override fun ${func.simpleName.asString()}(${func.parameters.map {"${it.name!!.asString()}: ${it.type.resolve().declaration.qualifiedName!!.asString()}"}.joinToString(", ")}) = TODO()"
-                    }
-                    .joinToString("\n")
-            }
+                    abstractFunctions
+                        .map { func ->
+                            "override fun ${func.simpleName.asString()}(${
+                                func.parameters.map { "${it.name!!.asString()}: ${it.type.resolve().declaration.qualifiedName!!.asString()}" }
+                                    .joinToString(", ")
+                            }) = TODO()"
+                        }
+                        .joinToString("\n")
+                }
                 }
             """.trimIndent().toByteArray()
-        )
-        output.close()
+            )
+            output.close()
+        }
     }
 
     override fun finish() {

d026082d196d98cfd844ea7af3c8252bc13fc732
Masatoshi Kubode <swi.masatoshi@gmail.com>
2021-08-22 23:34:17 +0900

    KotlinPoet 1.9.0

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 dependencies {
     implementation("com.google.devtools.ksp:symbol-processing-api:1.5.21-1.0.0-beta07")
     implementation("com.google.auto.service:auto-service-annotations:1.0")
-    implementation("com.squareup:kotlinpoet:1.7.2")
+    implementation("com.squareup:kotlinpoet:1.9.0")
     implementation(project(":api"))
     kapt("com.google.auto.service:auto-service:1.0")
 }

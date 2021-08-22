21e7fec7a013b49a6455d76c37afc2cb359c7831
Masatoshi Kubode <swi.masatoshi@gmail.com>
2021-08-22 22:43:13 +0900

    auto-service:1.0

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -3,16 +3,10 @@ plugins {
     kotlin("kapt")
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
 dependencies {
     implementation("com.google.devtools.ksp:symbol-processing-api:1.5.21-1.0.0-beta07")
-    implementation("com.google.auto.service:auto-service-annotations:1.0-rc7")
+    implementation("com.google.auto.service:auto-service-annotations:1.0")
     implementation("com.squareup:kotlinpoet:1.7.2")
     implementation(project(":annotation"))
-    kapt("com.google.auto.service:auto-service:1.0-rc7")
+    kapt("com.google.auto.service:auto-service:1.0")
 }

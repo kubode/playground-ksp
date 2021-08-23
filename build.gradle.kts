5ffa8c46530de44c5c5074c6d879525bc375495e
Masatoshi Kubode <swi.masatoshi@gmail.com>
2021-08-23 23:36:10 +0900

    Configure plugin on settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,4 @@
-buildscript {
-    repositories {
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath(kotlin("gradle-plugin", version = "1.5.21"))
-    }
-}
-
 subprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-
     group = "org.github.kubode"
     version = "1.0.0-SNAPSHOT"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,31 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    plugins {
+        kotlin("gradle-plugin")
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace in setOf("org.jetbrains.kotlin", "org.jetbrains.kotlin.plugin")) {
+                useVersion("1.5.21")
+            }
+        }
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "playground-ksp"
 
 include("api")
 include("app")
 include("processor")
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-    }
-}
